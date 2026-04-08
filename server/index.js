04df0cd654f83ee92c72e3c93c4098130f160e28
Harshit Rai <harshit143rai@gmail.com>
2026-04-08 14:11:50 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-mock-interview-1-ejba.onrender.com",
     credentials:true
 }))
 
